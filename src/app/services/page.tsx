6913905a0af6a192b84c6a22b3a5a84c8252cfe3
USBAI <usbai.contact@gmail.com>
2024-10-26 20:45:30 +0200

    Updating the services page, improved the page css and the content

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -12,38 +12,175 @@ export default function Services() {
     return(
         <>
             <NavBar />
+            <div className="margin__ijce9hf">
+                <br />
+            </div>
+            
+            <div className="mainheader_aboutwhatweprovide">
+                <h1>Our Services</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione itaque fugit consequatur, est dicta dolorum? Nemo, nostrum vero tempora omnis ullam quis, laborum quam officia quas delectus reiciendis pariatur vitae?</p>
+            </div>
             <div className="__ourservices_49jf9">
                 <div>
                     <div>
-                        <h1>Our Services</h1>
-                    </div>
-                    <div>
                         <div className="__cardslists_denf9">
                             <div className="__Savings_cost_avoidance">
-                                <div>
-                                    <img src="https://cdn.prod.website-files.com/631606e83c13edc22cfb6d57/65f027f41dc52076020a58b8_savings%201-p-800.jpg" alt="" />
+                                <div className="__Services_details__fef9_img">
+                                    <img className="__3service_img" src="https://cdn.prod.website-files.com/631606e83c13edc22cfb6d57/65f027f41dc52076020a58b8_savings%201-p-800.jpg" alt="" />
                                     <p className="pricing_d9jdf">90 000 SEK</p>
                                 </div>
-                                <div>
-                                    <h1>Savings & Cost avoidance</h1>
+                                <div className="__Services_details__fef9">
+                                    <div>
+                                        <h1>Savings & Cost avoidance</h1>
+                                    </div>
+                                    <div className="ratings_div_e3j">
+                                        <p>Ratings</p>
+                                        <div className="ratings_d9ej">
+                                            <svg width="68" height="12" viewBox="0 0 68 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                <g clip-path="url(#clip0_1_9)">
+                                                <path d="M6 1L7.545 4.13L11 4.635L8.5 7.07L9.09 10.51L6 8.885L2.91 10.51L3.5 7.07L1 4.635L4.455 4.13L6 1Z" fill="#F7B559" stroke="#F7B559" stroke-linecap="round" stroke-linejoin="round"/>
+                                                </g>
+                                                <g clip-path="url(#clip1_1_9)">
+                                                <path d="M20 1L21.545 4.13L25 4.635L22.5 7.07L23.09 10.51L20 8.885L16.91 10.51L17.5 7.07L15 4.635L18.455 4.13L20 1Z" fill="#F7B559" stroke="#F7B559" stroke-linecap="round" stroke-linejoin="round"/>
+                                                </g>
+                                                <g clip-path="url(#clip2_1_9)">
+                                                <path d="M34 1L35.545 4.13L39 4.635L36.5 7.07L37.09 10.51L34 8.885L30.91 10.51L31.5 7.07L29 4.635L32.455 4.13L34 1Z" fill="#F7B559" stroke="#F7B559" stroke-linecap="round" stroke-linejoin="round"/>
+                                                </g>
+                                                <g clip-path="url(#clip3_1_9)">
+                                                <path d="M48 1L49.545 4.13L53 4.635L50.5 7.07L51.09 10.51L48 8.885L44.91 10.51L45.5 7.07L43 4.635L46.455 4.13L48 1Z" fill="#F7B559" stroke="#F7B559" stroke-linecap="round" stroke-linejoin="round"/>
+                                                </g>
+                                                <g clip-path="url(#clip4_1_9)">
+                                                <path d="M62 1L63.545 4.13L67 4.635L64.5 7.07L65.09 10.51L62 8.885L58.91 10.51L59.5 7.07L57 4.635L60.455 4.13L62 1Z" stroke="#F7B559" stroke-linecap="round" stroke-linejoin="round"/>
+                                                </g>
+                                                <defs>
+                                                <clipPath id="clip0_1_9">
+                                                <rect width="12" height="12" fill="white"/>
+                                                </clipPath>
+                                                <clipPath id="clip1_1_9">
+                                                <rect width="12" height="12" fill="white" transform="translate(14)"/>
+                                                </clipPath>
+                                                <clipPath id="clip2_1_9">
+                                                <rect width="12" height="12" fill="white" transform="translate(28)"/>
+                                                </clipPath>
+                                                <clipPath id="clip3_1_9">
+                                                <rect width="12" height="12" fill="white" transform="translate(42)"/>
+                                                </clipPath>
+                                                <clipPath id="clip4_1_9">
+                                                <rect width="12" height="12" fill="white" transform="translate(56)"/>
+                                                </clipPath>
+                                                </defs>
+                                            </svg>
+                                        </div>
+                                    </div>
+                                    <div className="__addtocart__" id="componat__Savings">
+                                        <p>ADD TO CART</p>
+                                    </div>
                                 </div>
                             </div>
                             <div className="Code_of_Conduct">
-                                <div>
-                                    <img src="https://cdn.prod.website-files.com/631606e83c13edc22cfb6d57/65f027eda059f7aa5b4f99de_terms%20%26%20conditions%201-p-800.jpg" alt="" />
+                                <div className="__Services_details__fef9_img">
+                                    <img className="__3service_img" src="https://cdn.prod.website-files.com/631606e83c13edc22cfb6d57/65f027eda059f7aa5b4f99de_terms%20%26%20conditions%201-p-800.jpg" alt="" />
                                     <p className="pricing_d9jdf">90 000 SEK</p>
                                 </div>
-                                <div>
-                                    <h1>Code of Conduct</h1>
+                                <div className="__Services_details__fef9">
+                                    <div>
+                                        <h1>Code of Conduct</h1>
+                                    </div>
+                                    <div className="ratings_div_e3j">
+                                        <p>Ratings</p>
+                                        <div className="ratings_d9ej">
+                                            <svg width="68" height="12" viewBox="0 0 68 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                <g clip-path="url(#clip0_1_9)">
+                                                <path d="M6 1L7.545 4.13L11 4.635L8.5 7.07L9.09 10.51L6 8.885L2.91 10.51L3.5 7.07L1 4.635L4.455 4.13L6 1Z" fill="#F7B559" stroke="#F7B559" stroke-linecap="round" stroke-linejoin="round"/>
+                                                </g>
+                                                <g clip-path="url(#clip1_1_9)">
+                                                <path d="M20 1L21.545 4.13L25 4.635L22.5 7.07L23.09 10.51L20 8.885L16.91 10.51L17.5 7.07L15 4.635L18.455 4.13L20 1Z" fill="#F7B559" stroke="#F7B559" stroke-linecap="round" stroke-linejoin="round"/>
+                                                </g>
+                                                <g clip-path="url(#clip2_1_9)">
+                                                <path d="M34 1L35.545 4.13L39 4.635L36.5 7.07L37.09 10.51L34 8.885L30.91 10.51L31.5 7.07L29 4.635L32.455 4.13L34 1Z" fill="#F7B559" stroke="#F7B559" stroke-linecap="round" stroke-linejoin="round"/>
+                                                </g>
+                                                <g clip-path="url(#clip3_1_9)">
+                                                <path d="M48 1L49.545 4.13L53 4.635L50.5 7.07L51.09 10.51L48 8.885L44.91 10.51L45.5 7.07L43 4.635L46.455 4.13L48 1Z" fill="#F7B559" stroke="#F7B559" stroke-linecap="round" stroke-linejoin="round"/>
+                                                </g>
+                                                <g clip-path="url(#clip4_1_9)">
+                                                <path d="M62 1L63.545 4.13L67 4.635L64.5 7.07L65.09 10.51L62 8.885L58.91 10.51L59.5 7.07L57 4.635L60.455 4.13L62 1Z" stroke="#F7B559" stroke-linecap="round" stroke-linejoin="round"/>
+                                                </g>
+                                                <defs>
+                                                <clipPath id="clip0_1_9">
+                                                <rect width="12" height="12" fill="white"/>
+                                                </clipPath>
+                                                <clipPath id="clip1_1_9">
+                                                <rect width="12" height="12" fill="white" transform="translate(14)"/>
+                                                </clipPath>
+                                                <clipPath id="clip2_1_9">
+                                                <rect width="12" height="12" fill="white" transform="translate(28)"/>
+                                                </clipPath>
+                                                <clipPath id="clip3_1_9">
+                                                <rect width="12" height="12" fill="white" transform="translate(42)"/>
+                                                </clipPath>
+                                                <clipPath id="clip4_1_9">
+                                                <rect width="12" height="12" fill="white" transform="translate(56)"/>
+                                                </clipPath>
+                                                </defs>
+                                            </svg>
+                                        </div>
+                                    </div>
+                                    <div className="__addtocart__" id="componat__Conduct">
+                                        <p>ADD TO CART</p>
+                                    </div>
                                 </div>
                             </div>
                             <div className="ESG_measurement">
-                                <div>
-                                    <img src="https://cdn.prod.website-files.com/631606e83c13edc22cfb6d57/65b2465a479e2daaf82cab42_ESG-p-800.jpg" alt="" />
+                                <div className="__Services_details__fef9_img">
+                                    <img className="__3service_img" src="https://cdn.prod.website-files.com/631606e83c13edc22cfb6d57/65b2465a479e2daaf82cab42_ESG-p-800.jpg" alt="" />
                                     <p className="pricing_d9jdf">90 000 SEK</p>
                                 </div>
-                                <div>
-                                    <h1>ESG measurement</h1>
+                                <div className="__Services_details__fef9">
+                                    <div>
+                                        <h1>ESG measurement</h1>
+                                    </div>
+                                    <div className="ratings_div_e3j">
+                                        <p>Ratings</p>
+                                        <div className="ratings_d9ej">
+                                            <svg width="68" height="12" viewBox="0 0 68 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                <g clip-path="url(#clip0_1_9)">
+                                                <path d="M6 1L7.545 4.13L11 4.635L8.5 7.07L9.09 10.51L6 8.885L2.91 10.51L3.5 7.07L1 4.635L4.455 4.13L6 1Z" fill="#F7B559" stroke="#F7B559" stroke-linecap="round" stroke-linejoin="round"/>
+                                                </g>
+                                                <g clip-path="url(#clip1_1_9)">
+                                                <path d="M20 1L21.545 4.13L25 4.635L22.5 7.07L23.09 10.51L20 8.885L16.91 10.51L17.5 7.07L15 4.635L18.455 4.13L20 1Z" fill="#F7B559" stroke="#F7B559" stroke-linecap="round" stroke-linejoin="round"/>
+                                                </g>
+                                                <g clip-path="url(#clip2_1_9)">
+                                                <path d="M34 1L35.545 4.13L39 4.635L36.5 7.07L37.09 10.51L34 8.885L30.91 10.51L31.5 7.07L29 4.635L32.455 4.13L34 1Z" fill="#F7B559" stroke="#F7B559" stroke-linecap="round" stroke-linejoin="round"/>
+                                                </g>
+                                                <g clip-path="url(#clip3_1_9)">
+                                                <path d="M48 1L49.545 4.13L53 4.635L50.5 7.07L51.09 10.51L48 8.885L44.91 10.51L45.5 7.07L43 4.635L46.455 4.13L48 1Z" fill="#F7B559" stroke="#F7B559" stroke-linecap="round" stroke-linejoin="round"/>
+                                                </g>
+                                                <g clip-path="url(#clip4_1_9)">
+                                                <path d="M62 1L63.545 4.13L67 4.635L64.5 7.07L65.09 10.51L62 8.885L58.91 10.51L59.5 7.07L57 4.635L60.455 4.13L62 1Z" stroke="#F7B559" stroke-linecap="round" stroke-linejoin="round"/>
+                                                </g>
+                                                <defs>
+                                                <clipPath id="clip0_1_9">
+                                                <rect width="12" height="12" fill="white"/>
+                                                </clipPath>
+                                                <clipPath id="clip1_1_9">
+                                                <rect width="12" height="12" fill="white" transform="translate(14)"/>
+                                                </clipPath>
+                                                <clipPath id="clip2_1_9">
+                                                <rect width="12" height="12" fill="white" transform="translate(28)"/>
+                                                </clipPath>
+                                                <clipPath id="clip3_1_9">
+                                                <rect width="12" height="12" fill="white" transform="translate(42)"/>
+                                                </clipPath>
+                                                <clipPath id="clip4_1_9">
+                                                <rect width="12" height="12" fill="white" transform="translate(56)"/>
+                                                </clipPath>
+                                                </defs>
+                                            </svg>
+                                        </div>
+                                    </div>
+                                    <div className="__addtocart__" id="componat__measurement">
+                                        <p>ADD TO CART</p>
+                                    </div>
                                 </div>
                             </div>
                         </div>
